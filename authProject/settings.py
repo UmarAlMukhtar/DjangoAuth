f2d4f43c6b5aad114a40bad24c61fca9ae119fb2
Umar Al Mukhtar <umar1868807@gmail.com>
2025-07-17 13:30:32 +0530

    add allowed host

--- authProject/settings.py
+++ authProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1e!b0zu*w+*z5#g-7#4s83!12ooz=($&&2z9weqat@@h32xw^e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoauth-4wc3.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -116,12 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'images/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "authApp", "static"),
+]
 
 LOGIN_REDIRECT_URL = 'home'
 
